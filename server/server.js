05c90e1adee473ff8812f5ed394267740e7985aa
malek-d <malek.darwiche2@gmail.com>
2018-08-05 20:37:09 +1000

    Add mongoose database code

--- server/server.js
+++ server/server.js
@@ -1,15 +1,38 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
-import mongoose from 'mongoose';
+import mongoose, { Model, Schema } from 'mongoose';
 
 const app = express();
 const router = express.Router();
-
 const API_PORT = process.env.API_PORT || 3001;
 
+//temporary way to connect our db
+mongoose.connect('mongodb://MDarwiche:Kelam123!@ds111562.mlab.com:11562/clubbin');
+
+//start off with testing basics
+let userSchema = new mongoose.Schema({
+    name: String,
+    email: String
+});
+let users = mongoose.model('Users', userSchema, 'users');
+
+let getUsers = function(callback){
+    users.find(function(err, userss){
+        if(err) return console.error(err);
+        //console.log(userss);
+        callback(userss);
+    })
+}
+
+
 router.get('/', (req, res) => {
-    res.json({ success: true, data: { "test": "sample" }});
+    getUsers(function(result){
+        //get callback then display
+        res.send(result);
+    })
+    //res.json(getUsers());
+    //getUsers();
 });
 
 app.use('/api', router);
