9a8ff93bb778e302825c9a132b2a404ef1f5335d
rudaiyap <ramu.udai20@gmail.com>
2018-08-12 18:33:28 +1000

    Add Containers

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './containers/home'
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<div></div>, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 registerServiceWorker();
