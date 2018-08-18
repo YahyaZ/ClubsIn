f3cd1d3d0dcb55bba598c4ab59fbc3ff468e5bd9
malek-d <malek.darwiche2@gmail.com>
2018-08-18 13:18:30 +1000

    Add comments in server.js

--- server/server.js
+++ server/server.js
@@ -10,15 +10,23 @@ const API_PORT = process.env.API_PORT || 3001;
 //temporary way to connect our db
 mongoose.connect('mongodb://admin:Password123!@ds111562.mlab.com:11562/clubbin');
 
-//start off with testing basics
+/**
+ * Create a schema for users when connecting to our mongo database
+ */
 let userSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
     email: String,
     password: String
 });
+
+//User collection which handles any retrievals or changes in our user section
 let users = mongoose.model('Users', userSchema, 'users');
 
+/**
+ * Returns all users within the collection and returns it in a callback (async)
+ * @param {*} callback 
+ */
 let getUsers = function(callback){
     users.find(function(err, userss){
         if(err) return console.error(err);
@@ -27,6 +35,15 @@ let getUsers = function(callback){
     })
 }
 
+/**
+ * Creates a new user based off of our 'userSchema' model
+ * saves that with values based off parameters
+ * @param {*} callback 
+ * @param {*} first 
+ * @param {*} last 
+ * @param {*} email 
+ * @param {*} password 
+ */
 let registerUser = function(callback, first, last, email, password){
     var newUser = new users({firstName: first, lastName: last, email: email, password: password});
     newUser.save(function (err){
@@ -35,6 +52,12 @@ let registerUser = function(callback, first, last, email, password){
     })
 }
 
+/**
+ * Tries to find a single user within the collection
+ * @param {*} callback 
+ * @param {*} email 
+ * @param {*} password 
+ */
 let findUser = function(callback, email, password){
     users.findOne({email: email, password: password}, function(err,doc){
         if(err) callback(err);
@@ -42,6 +65,11 @@ let findUser = function(callback, email, password){
     });
 };
 
+/**
+ * localhost:3001/api/login
+ * call's finduser based on provided parameters
+ * MOCK DATA FOR NOW
+ */
 router.post('/login', (req, res) =>{
     findUser(function(result){
         try {
@@ -52,6 +80,11 @@ router.post('/login', (req, res) =>{
     }, "Yahyaiscool@cool.com", "touchmeDaddy");
 });
 
+/**
+ * localhost:3001/api/signup
+ * Registers a user based on provided parameters
+ * MOCK DATA
+ */
 router.post('/signup', (req, res) =>{
     registerUser(function(result){
         res.send(result);
