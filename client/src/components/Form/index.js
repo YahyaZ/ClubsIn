33b70c51bb1e50b4ab6c0f2caf8cc338ebfbacd2
rudaiyap <ramu.udai20@gmail.com>
2018-08-19 12:31:28 +1000

    Add password visibility button

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Logo from '../LOGO/logo.png'
-import { FaEnvelope, FaLock, FaEye } from "react-icons/fa"
+import { FaEnvelope, FaLock, FaEye, FaEyeSlash } from "react-icons/fa"
 
 const Header = (props) => (   
         <div className="form-header">
@@ -9,33 +9,59 @@ const Header = (props) => (
         </div>
     );
 
-const FormBody = (props) => (
-    <form className="form-body">
-        <div className="input-container">
-            <FaEnvelope className="icon"/>
-            <input className="input-field" type="text" placeholder="email" name="email" />
-        </div>
-        <div className="input-container">
-            <FaLock className="icon"/>
-            <input className="input-field" type="password" placeholder="password" name="password" />
-            <FaEye className="icon show-password"/>
-        </div>
-        <div className="form-body-footer" >
-            <div className="form-checkbox">
-                <input type="checkbox" name="RememberMe" />
-                <label>Remember Me</label>
-            </div>
-            <div className="form-bottom-left">
-                Forgot Password?
-            </div>
-        </div>
-        <button className="form-button">Log In</button>
-    </form>
-)
-
 const Footer = (props) => (
-   <div className="form-footer">{props.text} <a href={props.link} className="link">{props.linkText}</a></div>
-)
+        <div className="form-footer">{props.text} <a href={props.link} className="link">{props.linkText}</a></div>
+     )
+
+class FormBody extends Component{
+        constructor(props){
+            super(props);
+            this.state = {
+                type: 'password'
+            }
+            this.showHidePassword = this.showHidePassword.bind(this);
+        }
+
+
+        showHidePassword (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            this.setState({
+                type: this.state.type === 'input' ? 'password' : 'input'
+            }) 
+        }
+        
+        render(){
+            return (
+                <form className="form-body">
+                    <div className="input-container">
+                        <FaEnvelope className="icon"/>
+                        <input className="input-field" type="text" placeholder="email" name="email" />
+                    </div>
+                    <div className="input-container">
+                        <FaLock className="icon"/>
+                        <input className="input-field" type={this.state.type} placeholder="password" name="password" />
+                        <div className="icon show-password" onMouseDown={this.showHidePassword} onMouseUp={this.showHidePassword}>
+                            {this.state.type === 'input' ? <FaEye /> : <FaEyeSlash />}
+                        </div>
+                    </div>
+                    <div className="form-body-footer" >
+                        <div className="form-checkbox">
+                            <input type="checkbox" name="RememberMe" />
+                            <label>Remember Me</label>
+                        </div>
+                        <div className="form-bottom-left link">
+                            Forgot Password?
+                        </div>
+                    </div>
+                    <button className="form-button">Log In</button>
+                </form>
+            )
+    }
+}
+
+
+
 
 class Form extends Component {
     render() {
