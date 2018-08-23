9a7df349d6f7bee6f515e3348e4595f2f284cdeb
Yahya Z <yahyaz998@gmail.com>
2018-08-23 16:26:02 +1000

    Fix club sign up pages

--- client/src/components/SignUp/SignUpFormNewClub.js
+++ client/src/components/SignUp/SignUpFormNewClub.js
@@ -1,36 +1,41 @@
 import React, { Component } from "react";
 import { FaUniversity} from "react-icons/fa";
+import { FormGroup, InputGroup, FormControl } from "react-bootstrap";
 
 class SignUpFormNewClub extends Component{    
     render(){
         return (
             <form className="form-body">
-                <div className="input-container">
-                    <FaUniversity className="icon"/>
-                    <select className="input-field" name="university" >
-                        <option value="" disabled selected>University/College</option>
-                        <option value="UTS">UTS </option>
-                        <option value="UNSW">UNSW </option>
-                    </select>
-                </div>
-                <div className="input-container">
-                    <FaUniversity className="icon"/>
-                    <input className="input-field" type="text" placeholder="Club Name" name="clubName" />
-                </div>
-                <div className="input-container">
-                    <FaUniversity className="icon"/>
-                    <select className="input-field" name="clubType" >
-                        <option value="" disabled selected>Club Type</option>
-                        <option value="Religious">Religious </option>
-                        <option value="Faculty">Faculty </option>
-                        <option value="Culture">Culture </option>
-                        <option value="Sports">Sports </option>
-                        <option value="Gaming">Gaming</option>
-                        <option value="Creative">Creative</option>
-                        <option value="Politcal">Politcal </option>
-                        <option value="Social Justice">Social Justice</option>
-                    </select>
-                </div>
+                <FormGroup controlId="universityControlsSelect">
+                    <InputGroup>
+                        <InputGroup.Addon><FaUniversity/></InputGroup.Addon>
+                        <FormControl componentClass="select" placeholder="University/College" name="university">
+                            <option value="UTS">UTS</option>
+                            <option value="UNSW">UNSW</option>
+                        </FormControl>
+                    </InputGroup>
+                </FormGroup>
+                <FormGroup>
+                    <InputGroup>
+                        <InputGroup.Addon><FaUniversity/></InputGroup.Addon>
+                        <FormControl type="text" placeholder="Club Name" name="clubName" />
+                    </InputGroup>
+                </FormGroup>
+                <FormGroup controlId="clubTypeControlsSelect">
+                    <InputGroup>
+                        <InputGroup.Addon><FaUniversity/></InputGroup.Addon>
+                        <FormControl componentClass="select" placeholder="Club Type" name="clubType">
+                            <option value="Religious">Religious </option>
+                            <option value="Faculty">Faculty </option>
+                            <option value="Culture">Culture </option>
+                            <option value="Sports">Sports </option>
+                            <option value="Gaming">Gaming</option>
+                            <option value="Creative">Creative</option>
+                            <option value="Politcal">Politcal </option>
+                            <option value="Social Justice">Social Justice</option>
+                        </FormControl>    
+                    </InputGroup>
+                </FormGroup>
                 <button className="form-button" onClick={this.props.buttonClick}>Continue</button>
             </form>
         )
--- client/src/components/SignUp/signUpFormExistingClub.js
+++ client/src/components/SignUp/signUpFormExistingClub.js
@@ -1,23 +1,27 @@
 import React, { Component } from "react";
 import { FaUniversity } from "react-icons/fa";
+import { FormGroup, InputGroup, FormControl } from 'react-bootstrap';
 
 class SignUpFormExistingClub extends Component{
    
     render(){
         return (
             <form className="form-body">
-                <div className="input-container">
-                    <FaUniversity className="icon"/>
-                    <select className="input-field" name="university" >
-                        <option value="" disabled selected>University/College</option>
-                        <option value="UTS">UTS </option>
-                        <option value="UNSW">UNSW </option>
-                    </select>
-                </div>
-                <div className="input-container">
-                    <FaUniversity className="icon"/>
-                    <input className="input-field" type="text" placeholder="Club Name" name="clubName" />
-                </div>
+                <FormGroup controlId="universityControlsSelect">
+                    <InputGroup>
+                        <InputGroup.Addon><FaUniversity/></InputGroup.Addon>
+                        <FormControl componentClass="select" placeholder="University/College">
+                            <option value="UTS">UTS</option>
+                            <option value="UNSW">UNSW</option>
+                        </FormControl>
+                    </InputGroup>
+                </FormGroup>
+                <FormGroup>
+                    <InputGroup>
+                        <InputGroup.Addon><FaUniversity/></InputGroup.Addon>
+                        <FormControl type="text" placeholder="Club Name" name="clubName" />
+                    </InputGroup>
+                </FormGroup>
                 <button className="form-button" onClick={this.props.buttonClick}>Continue</button>
             </form>
         )
