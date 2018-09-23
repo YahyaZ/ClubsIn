a2c13cad744874eb09919eeb30da9256d96edb67
YahyaZ <yahyaz998@gmail.com>
2018-09-23 20:07:24 +1000

    Add created by and last modified fields in events model

--- server/model/events.js
+++ server/model/events.js
@@ -4,7 +4,12 @@ let eventSchema = new mongoose.Schema({
     club_id: {type: Schema.Types.ObjectId, ref:'clubs'},
     name: String,
     description: String,
-    date: Date
+    date: Date,
+    created_by: {
+        type: Schema.Types.ObjectId, 
+        ref: 'users'
+    },
+    last_modified: Date
 });
 
 var Events = mongoose.model('events', eventSchema);
