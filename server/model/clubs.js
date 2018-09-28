e0a9f21857257918abfcae3c7e72bb252bc68b03
malek-d <malek.darwiche2@gmail.com>
2018-09-28 17:10:41 +1000

    Add validation to clubs

--- server/model/clubs.js
+++ server/model/clubs.js
@@ -2,9 +2,21 @@ import mongoose from "mongoose";
 import User from './users'
 
 let ClubSchema = new mongoose.Schema({
-    name: String,
-    type: String,
-    university: String,
+    name: {
+      type: String,
+      required:[true, 'Club name can not be blank'],
+      trim: true
+    },
+    type: {
+      type: String,
+      required: [true, 'University type can not be blank'],
+      trim:  true
+    },
+    university: {
+      type: String,
+      required:[true, 'University field cannot be blank'],
+      trim: true
+    },
     users:[{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
--- server/services/clubs.js
+++ server/services/clubs.js
@@ -1,33 +1,37 @@
 import Club from '../model/clubs';
 
 function createClub(req, res, next) {
-    var clubData = {
-        name: req.body.clubName,
-        type: req.body.clubType,
-        university: req.body.university,
+    if(req.body.clubName && req.body.clubType && req.body.university){
+        var clubData = {
+            name: req.body.clubName,
+            type: req.body.clubType,
+            university: req.body.university,
+        }
+    
+        // Checks if the club is existing or not, if it doesnt, add it
+        Club.findOne({ name: clubData.name, university: clubData.university }, function (err, club) {
+            if (err) {
+                console.log(err);
+            }
+            // Club Exists, so return Club Already exists
+            if (club) {
+                res.status(400).json({ "error": "Club Already Exists" })
+            } else {
+                // No club is found, so create it
+                Club.create(clubData, function (error, club) {
+                    if (error) {
+                        error.status = 400;
+                        return next(error);
+                    } else {
+                        req.session.clubId = club._id;
+                        return res.json(club)
+                    }
+                });
+            }
+        });
+    } else {
+        res.status(200).json({"error": "All Fields required"})
     }
-
-    // Checks if the club is existing or not, if it doesnt, add it
-    Club.findOne({ name: clubData.name, university: clubData.university }, function (err, club) {
-        if (err) {
-            console.log(err);
-        }
-        // Club Exists, so return Club Already exists
-        if (club) {
-            res.status(400).json({ "error": "Club Already Exists" })
-        } else {
-            // No club is found, so create it
-            Club.create(clubData, function (error, club) {
-                if (error) {
-                    error.status = 400;
-                    return next(error);
-                } else {
-                    req.session.clubId = club._id;
-                    return res.json(club)
-                }
-            });
-        }
-    });
 }
 
 
@@ -43,42 +47,59 @@ function getClubs(res){
  * This function is mainly used when trying to find a club to join for newly registered users
  */
 function findClub(res,req){
-    clubs.findOne({name: req.body.name, university: req.body.university},
-        (err, doc) => {
-        if(err) console.error(err);
-        res.json(doc);
-    });
+    if(req.body.name && req.body.university){
+        clubs.findOne({name: req.body.name, university: req.body.university},
+            (err, doc) => {
+            if(err) console.error(err);
+            res.json(doc);
+        });
+    } else {
+        res.status(200).json({"error": "Fields missing"})
+    }
 }
 
+
 function findClubById(res,req, next){
-    clubs.findOne({_id: req.params.id},(err, doc) => {
-        if(err) next(err)
-        res.json(doc);
-    });
+    if(req.body.id){
+        clubs.findOne({_id: req.params.id},(err, doc) => {
+            if(err) next(err)
+            res.status(200).json(doc);
+        });
+    } else {
+        res.status(400).json({"error": "ID not provided"})
+    }
 }
 
 //Create
-function createClub(req, res){
-    new clubs({name: req.body.name, type: req.body.type, university: req.body.university, events: req.body.events })
-    .save(function(err){
-        if(err) console.error(err);
-        res.json(`Success`);
-    });
+function createClub(req, res, next){
+    if(req.body.name && req,body.type && req.body.university && req.body.events){
+        new clubs({name: req.body.name, type: req.body.type, university: req.body.university, events: req.body.events })
+        .save(function(err){
+            if(err) console.error(err);
+            res.json(`Success`);
+        });
+    } else {
+        res.status(400).json({"error": "All fields required"})
+    }
 }
 
 function updateClub(req,res,next) {
-    Clubs.findOneAndUpdate({_id: req.body._id}, {
-        name: req.body.name,
-        type: req.body.type,
-        university: req.body.university,
-        users: JSON.parse(req.body.events)
-    }, (err, club) =>{
-        if(err){
-            err.status = 404;
-            next(err);
-        }
-        res.status(200).json(club);
-    })
+    if(req.body.id){
+        Clubs.findOneAndUpdate({_id: req.body._id}, {
+            name: req.body.name,
+            type: req.body.type,
+            university: req.body.university,
+            users: JSON.parse(req.body.events)
+        }, (err, club) =>{
+            if(err){
+                err.status = 404;
+                next(err);
+            }
+            res.status(200).json(club);
+        })
+    } else {
+        res.status(400).json({"error":"ID not found"}) 
+    }
 }
 
 module.exports = {
