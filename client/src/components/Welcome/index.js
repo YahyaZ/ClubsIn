8754c8a00fd2bf54e51ab58df92f8dd66da6a73b
rudaiyap <ramu.udai20@gmail.com>
2018-10-10 00:53:59 +1100

    fix css changes on welcome box

--- client/src/components/Welcome/index.js
+++ client/src/components/Welcome/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import {Jumbotron, Button} from 'react-bootstrap';
+import './welcome.css'
 
 
 
 const Welcome = () => (
-    <div className="jumbotron jumbotron-fluid" style={{textAlign: 'center', backgroundColor:'transparent'}}>
+    <div className="whiteText jumbotron jumbotron-fluid" style={{textAlign: 'center', backgroundColor:'transparent'}}>
         <h1>Club'in</h1>
         <p>
             Makes your university club management easy and efficient, so you can spend more time having fun!
