8573f2e01f66a053525074365bd8343ec2ad913b
malek-d <malek.darwiche2@gmail.com>
2018-10-14 12:40:16 +1100

    Add new error handling class

---
+++ server/services/userErrorUtils.js
@@ -0,0 +1,25 @@
+function createError(message, code){
+    const error = new Error(message);
+    error.status = code;
+    return error;
+}
+
+let errorMessages = {
+    USER_NOT_FOUND: 'User does not exist in the dBase, please sign up to login as a user',
+    PASSWORD_MISMATCH: 'Passwords do not match',
+    EXISTING_USER: 'User Already Exists',
+    MISSING_FIELDS: 'Please fill out all fields',
+    INCORRECT_EMAIL_PASS: 'Wrong email or password',
+    TASK_NOT_FOUND: 'No task found',
+    EVENT_NOT_FOUND: 'Event not found',
+    EVENT_UPDATED: 'Event Updated',
+    CLUB_EXISTS: 'Club already exists',
+    USER_IN_CLUB: 'User is already in club',
+    INVALID_LINK: 'Invite Link Invalid',
+    NO_ID: 'ID not provided'
+}
+
+module.exports = {
+    createError,
+    errorMessages,
+}
